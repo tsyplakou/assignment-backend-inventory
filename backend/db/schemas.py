c002042c9b0e5f528d935988be71832346f71b6b
“Yahor <yahor.tsyplakou@gmail.com>
2022-06-28 19:47:39 +0200

    Update schemas for all models.

--- backend/db/schemas.py
+++ backend/db/schemas.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, constr, Field
+from datetime import date
 
 
 class StorageSpaceBase(BaseModel):
-    name: constr(max_length=32)
+    name: str = Field(max_length=32)
     is_refrigerated: bool
     max_capacity: int
 
@@ -17,9 +18,57 @@ class StorageSpaceUpdate(StorageSpaceBase):
 
 class StorageSpace(StorageSpaceBase):
     id: int
-    name: constr(max_length=32)
+    name: str = Field(max_length=32)
     is_refrigerated: bool
     max_capacity: int
+
+    class Config:
+        orm_mode = True
+
+
+class ItemTypeBase(BaseModel):
+    name: str = Field(max_length=32)
+    is_kept_cold: bool
+
+
+class ItemTypeCreate(ItemTypeBase):
+    pass
+
+
+class ItemTypeUpdate(ItemTypeBase):
+    pass
+
+
+class ItemType(ItemTypeBase):
+    id: int
+    name: str = Field(max_length=32)
+    is_kept_cold: bool
+
+    class Config:
+        orm_mode = True
+
+
+class ItemBase(BaseModel):
+    name: str = Field(max_length=32)
+    expiration_date: date
+    item_type_id: int
+    storage_space_id: int
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class ItemUpdate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    name: str = Field(max_length=32)
+    expiration_date: date
+    item_type_id: int
+    storage_space_id: int
 
     class Config:
         orm_mode = True
