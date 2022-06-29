fc08299433e4ae94558eafb63a1fa0f66c4b5428
“Yahor <yahor.tsyplakou@gmail.com>
2022-06-29 15:53:01 +0200

    Add new test cases for StorageSpace routes.

--- backend/tests.py
+++ backend/tests.py
@@ -1,13 +1,13 @@
 import os
-import uuid
+from datetime import datetime, timedelta
+
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from storage.models import Base, StorageSpace, ItemType, Item
-from settings.dependencies import get_db, session
-
 from main import app
+from settings.dependencies import get_db, session
+from storage.models import Base, StorageSpace, ItemType, Item
 
 engine = create_engine(os.environ['DB_URL'], echo=True)
 Base.metadata.create_all(bind=engine)
@@ -27,9 +27,9 @@ client = TestClient(app)
 
 def clean_db():
     with session(bind=engine) as s:
+        s.query(Item).delete()
         s.query(StorageSpace).delete()
         s.query(ItemType).delete()
-        s.query(Item).delete()
 
 
 class TestStorageSpace:
@@ -44,12 +44,27 @@ class TestStorageSpace:
         with session(bind=engine) as s:
             for index in range(1, 3):
                 storage_space = StorageSpace(
-                    name=f'Space{index}',
+                    name=('Refrigerator', 'Shelves')[index-1],
                     is_refrigerated=bool(index % 2),
                     max_capacity=index * 5,
                 )
                 s.add(storage_space)
+                item_type = ItemType(
+                    name=('Ice-cream', 'Cookies')[index-1],
+                    is_kept_cold=bool(index % 2),
+                )
+                s.add(item_type)
+                s.commit()
+                s.refresh(storage_space)
+                s.refresh(item_type)
+                s.add(Item(
+                    name=('Ben & Jerry`s', 'Oreo')[index - 1],
+                    expiration_date=(datetime.now() + timedelta(days=3)).date(),
+                    item_type_id=item_type.id,
+                    storage_space_id=storage_space.id,
+                ))
         cls.storage_space = s.query(StorageSpace).first()
+        cls.related_item = s.query(Item).first()
 
     def test_storage_spaces_list(self):
         with engine.connect() as con:
@@ -83,3 +98,63 @@ class TestStorageSpace:
             'max_capacity': self.storage_space.max_capacity,
         }
 
+    def test_update_storage_space_validates_data_and_updates_instance(self):
+        response = client.put(
+            f'/storage_spaces/{self.storage_space.id}/',
+            json={
+                'name': self.storage_space.name + ' new',
+                'is_refrigerated': not self.storage_space.is_refrigerated,
+            }
+        )
+
+        assert response.status_code == 400
+        assert response.json() == {
+            'detail': (
+                'Storage space refregerating can\'t be updated when items '
+                'inside.'
+            ),
+        }
+
+        response = client.put(
+            f'/storage_spaces/{self.storage_space.id}/',
+            json={
+                'name': self.storage_space.name + ' new',
+                'is_refrigerated': self.storage_space.is_refrigerated,
+            }
+        )
+        assert response.status_code == 200
+        assert response.json()['name'] == self.storage_space.name + ' new'
+
+    def test_delete_storage_space_validates_data_and_deletes_instance(self):
+        response = client.delete(f'/storage_spaces/{self.storage_space.id}/')
+
+        assert response.status_code == 400
+        assert response.json() == {
+            'detail': 'Storage space contains items. It can\'t be deleted.'
+        }
+
+        with session(bind=engine) as s:
+            s.add(StorageSpace(
+                name='To be removed',
+                is_refrigerated=True,
+                max_capacity=10,
+            ))
+        response = client.delete(
+            '/storage_spaces/{}/'.format(s.query(StorageSpace).order_by(
+                StorageSpace.id.desc()
+            ).first().id)
+        )
+        assert response.status_code == 204
+
+
+    def test_read_items_in_storage_place_returns_list_of_related_items(self):
+        response = client.get(f'/storage_spaces/{self.storage_space.id}/items/')
+
+        assert response.status_code == 200
+        assert response.json() == [{
+            'id': self.related_item.id,
+            'name': self.related_item.name,
+            'expiration_date': self.related_item.expiration_date.isoformat(),
+            'item_type_id': self.related_item.item_type_id,
+            'storage_space_id': self.storage_space.id,
+        }]
